590c01261efc30015fb58681251be15d99a2e69e
Duc Tran <englishlayup@protonmail.com>
2022-04-15 18:59:49 -0400

    add markdown editor

---
+++ pages/write-a-post.js
@@ -0,0 +1,10 @@
+import MarkdownEditor from "../components/md-editor"
+import Layout from "../components/layout"
+
+export default function PostEditor() {
+    return (
+        <Layout>
+            <MarkdownEditor/>
+        </Layout>
+    )
+}
